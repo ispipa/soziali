3f65050d5c45e64e42365fbcc9aecb7c6f09259e
ispipa <ispipa123@gmail.com>
2023-01-04 20:11:53 +0100

    links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,18 +11,18 @@ const Nav = () => {
         <div className='content-nav'>
             <div className='nav'>
                 <div className='logo'>
-                    <a href='/'><h1><span className='welcome'>Soziali</span></h1></a>
+                    <Link to={'/'}><h1><span className='welcome'>Soziali</span></h1></Link>
                 </div>
                 <div className='containMU'>
                     <div className='menu'>
                         <ul>
-                            <li><a href='/'>Inicio</a></li>
-                            <li><a href='/contacto'>Contacto</a></li>
+                            <li><Link to={'/'}>Inicio</Link></li>
+                            <li><Link to={'/contacto'}>Contacto</Link></li>
                         </ul>
                     </div>
                     <div className='user'>
                         <div className='userSesion'>
-                            {isUser   ? <h4><span className='user-iconA'>{userName[0]}</span><a id='usuario-link' href="/perfil"><p>{userName}</p></a></h4> : <div><button><Link to="/login">Iniciar sesión</Link></button></div>}
+                            {isUser   ? <h4><span className='user-iconA'>{userName[0]}</span><Link id='usuario-link' to={"/perfil"}><p>{userName}</p></Link></h4> : <div><button><Link to="/login">Iniciar sesión</Link></button></div>}
                         </div>
                     </div>
                 </div>
